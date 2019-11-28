b6b3b808a88b5f243aa28c10b5e32e34a5994d73
tendyfan <fansonggu@qq.com>
2019-11-28 16:48:03 +0800

    配置中心服务端

--- src/main/java/com/example/tendy/configserver/ConfigServerApplication.java
+++ src/main/java/com/example/tendy/configserver/ConfigServerApplication.java
@@ -2,8 +2,13 @@ package com.example.tendy.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+/**
+ * @author tendy
+ */
 @SpringBootApplication
+@EnableConfigServer
 public class ConfigServerApplication {
 
     public static void main(String[] args) {
